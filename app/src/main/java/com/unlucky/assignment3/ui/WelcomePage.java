3e1cb126b4693a97f641963a9c433314ef6ae663
des <tugiadinh117@gmail.com>
2022-12-17 10:31:57 +0700

    can access welcome page

--- app/src/main/java/com/unlucky/assignment3/ui/WelcomePage.java
+++ app/src/main/java/com/unlucky/assignment3/ui/WelcomePage.java
@@ -1,20 +1,44 @@
 package com.unlucky.assignment3.ui;
 
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unlucky.assignment3.R;
+import com.unlucky.assignment3.databinding.ActivityWelcomePageBinding;
 
 public class WelcomePage extends AppCompatActivity {
 
-    Button signInButton;
+    private ActivityWelcomePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityWelcomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView signInNavigation = findViewById(R.id.sign_in_navigation);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_buyer_sign_in, R.id.nav_seller_sign_in)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.BuyerSellerFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(binding.SignInNavMenu, navController);
     }
 }
