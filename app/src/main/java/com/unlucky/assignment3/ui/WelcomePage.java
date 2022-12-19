5c659e26888a5f8553c2ac3c0ec2d71ee3ce80dd
quynguyn <s3878636@rmit.edu.vn>
2022-12-19 10:29:13 +0700

    menu

--- app/src/main/java/com/unlucky/assignment3/ui/WelcomePage.java
+++ app/src/main/java/com/unlucky/assignment3/ui/WelcomePage.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unlucky.assignment3.R;
-import com.unlucky.assignment3.databinding.ActivityWelcomePageBinding;
+
 import com.unlucky.assignment3.user.buyer.BuyerDetail;
 import com.unlucky.assignment3.user.buyer.BuyerMain;
 import com.unlucky.assignment3.user.buyer.BuyerPayment;
@@ -20,21 +25,6 @@ public class WelcomePage extends AppCompatActivity {
 
 //        Intent i = new Intent(this, BuyerPayment.class);
 //        startActivity(i);
-
-        binding = ActivityWelcomePageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView signInNavigation = findViewById(R.id.sign_in_navigation);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_buyer_sign_in, R.id.nav_seller_sign_in)
-                .build();
-
-        NavController navController = Navigation.findNavController(this, R.id.BuyerSellerFragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        NavigationUI.setupWithNavController(binding.SignInNavMenu, navController);
         setContentView(R.layout.activity_welcome_page);
     }
 }
