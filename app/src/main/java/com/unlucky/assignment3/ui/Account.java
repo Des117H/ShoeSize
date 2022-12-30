864d724eb9f7a587f46bb167e1cafd64176f70bf
quynguyn <s3878636@rmit.edu.vn>
2022-12-30 23:37:42 +0700

    bottom done for fun

---
+++ app/src/main/java/com/unlucky/assignment3/ui/Account.java
@@ -0,0 +1,16 @@
+package com.unlucky.assignment3.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unlucky.assignment3.R;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
--- app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerMain.java
+++ app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerMain.java
@@ -7,17 +7,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.unlucky.assignment3.R;
 import com.unlucky.assignment3.shoe.Shoe;
+import com.unlucky.assignment3.ui.Account;
 import com.unlucky.assignment3.utilities.adapter.ShoeRecyclerViewAdapter;
 
 import java.util.ArrayList;
@@ -131,5 +135,28 @@ public class BuyerMain extends AppCompatActivity {
                         }
                     }
                 });
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(BuyerMain.this,"Home",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_cart:
+                        Intent i = new Intent(BuyerMain.this, BuyerShoppingCart.class);
+                        startActivity(i);
+                        break;
+                    case R.id.action_account:
+                        Intent x = new Intent(BuyerMain.this, Account.class);
+                        startActivity(x);
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
+
 }
