3d0153e178c4a05120a35a271f6dccbdca33f104
des <tugiadinh117@gmail.com>
2023-01-09 22:26:38 +0700

    add log in and sign up using firebase, add map api

--- app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerPayment.java
+++ app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerPayment.java
@@ -1,19 +1,31 @@
 package com.unlucky.assignment3.user.buyer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.unlucky.assignment3.R;
 
-public class BuyerPayment extends AppCompatActivity {
-
+public class BuyerPayment extends AppCompatActivity implements OnMapReadyCallback {
     TextView price;
-     Button placeOrder;
+    Button placeOrder;
+    boolean confirm = true;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,21 +42,60 @@ public class BuyerPayment extends AppCompatActivity {
 
         Intent i = getIntent();
 
-        price  =findViewById(R.id.price);
+        price = findViewById(R.id.price);
         placeOrder = findViewById(R.id.placeOrder);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         double priceNumber = (double) i.getExtras().get("price");
         String total = "Total :$" + priceNumber;
         price.setText(total);
 
-        placeOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        placeOrder.setOnClickListener(view -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(BuyerPayment.this).create();
+
+            alertDialog.setTitle("Confirmation");
+            alertDialog.setMessage("Do you want to place this order?");
+
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Yes", (dialog, which) -> {
+                Toast.makeText(BuyerPayment.this, "Ordered", Toast.LENGTH_SHORT).show();
+                confirm = true;
+            });
+
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "No", (dialog, which) -> {
+                dialog.dismiss();
+                confirm = false;
+            });
+
+            alertDialog.show();
+
+            if (confirm) {
                 Intent y = new Intent(BuyerPayment.this,BuyerMain.class);
                 startActivity(y);
                 finish();
             }
         });
 
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
