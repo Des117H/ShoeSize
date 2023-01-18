ac2cdc08e915a8b78b198e7c87394e8e5c751ac6
quynguyn <s3878636@rmit.edu.vn>
2023-01-19 01:10:41 +0700

    able to search on location bar

--- app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerPayment.java
+++ app/src/main/java/com/unlucky/assignment3/user/buyer/BuyerPayment.java
@@ -50,6 +50,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import io.reactivex.rxjava3.functions.Consumer;
+
 public class BuyerPayment extends AppCompatActivity implements AdapterView.OnItemSelectedListener, OnMapReadyCallback, OnMapsSdkInitializedCallback {
     TextView price, searchText;
     Button placeOrder,search;
